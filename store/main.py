1ff8f51660b9c9449d6d0a824686f7f5617acb90
Dmytro Habaznia <karambolrul@gmail.com>
2024-02-23 23:09:50 +0200

    All done except update

--- store/main.py
+++ store/main.py
@@ -10,12 +10,12 @@ from sqlalchemy import (
     Integer,
     String,
     Float,
-    DateTime,
+    DateTime, update,
 )
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.sql import select
 from datetime import datetime
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, field_validator, Field
 from config import (
     POSTGRES_HOST,
     POSTGRES_PORT,
@@ -30,26 +30,33 @@ app = FastAPI()
 DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
-# Define the ProcessedAgentData table
-processed_agent_data = Table(
-    "processed_agent_data",
-    metadata,
-    Column("id", Integer, primary_key=True, index=True),
-    Column("road_state", String),
-    Column("user_id", Integer),
-    Column("x", Float),
-    Column("y", Float),
-    Column("z", Float),
-    Column("latitude", Float),
-    Column("longitude", Float),
-    Column("timestamp", DateTime),
-)
+
+
+Base = declarative_base()
+
+
+# Define SQLAlchemy ORM mapped class
+class ProcessedAgentDataDbModel(Base):
+    __tablename__ = 'processed_agent_data'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    road_state = Column(String)
+    user_id = Column(Integer)
+    x = Column(Float)
+    y = Column(Float)
+    z = Column(Float)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    timestamp = Column(DateTime)
+
+
+Base.metadata.create_all(engine)
 SessionLocal = sessionmaker(bind=engine)
 
 
 # SQLAlchemy model
 class ProcessedAgentDataInDB(BaseModel):
-    id: int
+    id: int = Field(default=None, allow_mutation=True)
     road_state: str
     user_id: int
     x: float
@@ -121,6 +128,19 @@ async def send_data_to_subscribers(user_id: int, data):
             await websocket.send_json(json.dumps(data))
 
 
+def convert_agent_data(raw_data: ProcessedAgentData):
+    return ProcessedAgentDataInDB(
+        road_state=raw_data.road_state,
+        user_id=raw_data.agent_data.user_id,
+        x=raw_data.agent_data.accelerometer.x,
+        y=raw_data.agent_data.accelerometer.y,
+        z=raw_data.agent_data.accelerometer.z,
+        latitude=raw_data.agent_data.gps.latitude,
+        longitude=raw_data.agent_data.gps.longitude,
+        timestamp=raw_data.agent_data.timestamp,
+    )
+
+
 # FastAPI CRUDL endpoints
 
 
@@ -128,8 +148,19 @@ async def send_data_to_subscribers(user_id: int, data):
 async def create_processed_agent_data(data: List[ProcessedAgentData]):
     # Insert data to database
     # Send data to subscribers
-    pass
+    with SessionLocal() as session:
+        converted_data = []
 
+        for dataItem in data:
+            converted_data.append(ProcessedAgentDataDbModel(**convert_agent_data(dataItem).dict()))
+
+        print(converted_data[0].latitude)
+
+        session.add_all(converted_data)
+        session.commit()
+
+        for processed_subscriber in subscriptions:
+            await send_data_to_subscribers(processed_subscriber, converted_data)
 
 @app.get(
     "/processed_agent_data/{processed_agent_data_id}",
@@ -137,22 +168,38 @@ async def create_processed_agent_data(data: List[ProcessedAgentData]):
 )
 def read_processed_agent_data(processed_agent_data_id: int):
     # Get data by id
-    pass
+    with SessionLocal() as session:
+        return session.query(ProcessedAgentDataDbModel).get(processed_agent_data_id)
 
 
 @app.get("/processed_agent_data/", response_model=list[ProcessedAgentDataInDB])
 def list_processed_agent_data():
     # Get list of data
-    pass
-
+    with SessionLocal() as session:
+        return session.query(ProcessedAgentDataDbModel).all()
 
 @app.put(
     "/processed_agent_data/{processed_agent_data_id}",
     response_model=ProcessedAgentDataInDB,
 )
-def update_processed_agent_data(processed_agent_data_id: int, data: ProcessedAgentData):
+async def update_processed_agent_data(processed_agent_data_id: int, data: ProcessedAgentData):
     # Update data
-    pass
+    # Insert data to database
+    # Send data to subscribers
+    with SessionLocal() as session:
+        updated_entry = session.query(ProcessedAgentDataDbModel).get(processed_agent_data_id)
+        if updated_entry is None:
+            return None
+
+        for key, value in convert_agent_data(data).model_dump():
+            setattr(updated_entry, key, value)
+
+        session.commit()
+
+        for subscriber_id in subscriptions:
+            await send_data_to_subscribers(subscriber_id, updated_entry)
+
+        return updated_entry
 
 
 @app.delete(
@@ -161,7 +208,9 @@ def update_processed_agent_data(processed_agent_data_id: int, data: ProcessedAge
 )
 def delete_processed_agent_data(processed_agent_data_id: int):
     # Delete by id
-    pass
+    with SessionLocal() as session:
+        session.query(ProcessedAgentDataDbModel).delete(processed_agent_data_id)
+        session.commit()
 
 
 if __name__ == "__main__":
